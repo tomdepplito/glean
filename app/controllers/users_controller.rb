655b673d24c40f541ee5c78c36a5541449abf611
tomdepplito <tomdepplito@gmail.com>
2014-10-25 18:25:21 -0400

    Added basic user signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(new_user_params)
+    if(@user.save)
+      redirect_to root_path
+    else
+      render :back
+    end
   end
 
   def new
